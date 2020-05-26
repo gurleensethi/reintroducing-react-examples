05317d201a01b82bf797242aee7c42bd13fcd3b3
Gurleen Sethi <sarusethi@rocketmail.com>
2020-05-25 22:26:08 -0400

    styling a component

--- src/8. React Patterns/CompoundComponents.tsx
+++ src/8. React Patterns/CompoundComponents.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent, createContext } from "react";
+import "./CompoundComponents.css";
 
 interface Props {
   onExpanded?: (expanded: boolean) => void;
@@ -38,23 +39,39 @@ const ExpandableComponent: FunctionComponent<Props> = ({
   );
 };
 
-const Header: FunctionComponent = ({ children }) => {
+const Header: FunctionComponent<React.ButtonHTMLAttributes<
+  HTMLButtonElement
+>> = ({ children, className, ...props }) => {
   const { toggle } = React.useContext(ExpandableContext);
-  return <div onClick={toggle}>{children}</div>;
+  return (
+    <button
+      onClick={toggle}
+      className={`Expandable-trigger ${className || ""}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
 
-const Body: FunctionComponent = ({ children }) => {
+const Body: FunctionComponent<React.HTMLAttributes<HTMLDivElement>> = ({
+  children,
+  className,
+  ...props
+}) => {
   const { expanded } = React.useContext(ExpandableContext);
   const divRef = React.useRef<HTMLDivElement | null>();
 
   return (
     <div
+      className={className}
       ref={(ref) => (divRef.current = ref)}
       style={{
         maxHeight: expanded ? `${divRef.current?.scrollHeight}px` : "0",
         overflow: "hidden",
         transition: "max-height 0.3s",
       }}
+      {...props}
     >
       {children}
     </div>
@@ -66,7 +83,24 @@ export const CompoundComponents: FunctionComponent = (props) => {
     <div>
       <ExpandableComponent>
         <Header>Click me I am a header!</Header>
-        <Body>I am a body!</Body>
+        <Body>
+          “
+          <img
+            src="https://i.imgur.com/qpj4Y7N.png"
+            style={{ width: "250px" }}
+            alt="reintroducing react book cover"
+          />
+          <p style={{ opacity: 0.7 }}>
+            This book is so f*cking amazing! <br />
+            <a
+              href="https://leanpub.com/reintroducing-react"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Go get it now.
+            </a>
+          </p>
+        </Body>
       </ExpandableComponent>
     </div>
   );
